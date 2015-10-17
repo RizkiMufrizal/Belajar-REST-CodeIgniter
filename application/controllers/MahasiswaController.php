ad899a7255648e29b43b02771aeddd8c598416ab
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-10-17 22:11:08 +0700

    tambah mahasiswa controller

---
+++ application/controllers/MahasiswaController.php
@@ -0,0 +1,73 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class MahasiswaController extends CI_Controller {
+
+  public function __construct()
+  {
+    parent::__construct();
+    $this->load->model('Mahasiswa');
+  }
+
+  public function getMahasiswa()
+  {
+    $response = $this->Mahasiswa->getMahasiswa()->result();
+
+    $this->output
+      ->set_status_header(200)
+      ->set_content_type('application/json', 'utf-8')
+      ->set_output(json_encode($response, JSON_PRETTY_PRINT))
+      ->_display();
+      exit;
+  }
+
+  public function saveMahasiswa()
+  {
+      $data = (array)json_decode(file_get_contents('php://input'));
+      $this->Mahasiswa->insertMahasiswa($data);
+
+      $response = array(
+        'Success' => true,
+        'Info' => 'Data Tersimpan');
+
+      $this->output
+        ->set_status_header(201)
+        ->set_content_type('application/json', 'utf-8')
+        ->set_output(json_encode($response, JSON_PRETTY_PRINT))
+        ->_display();
+        exit;
+  }
+
+  public function updateMahasiswa($npm)
+  {
+    $data = (array)json_decode(file_get_contents('php://input'));
+    $this->Mahasiswa->updateMahasiswa($data, $npm);
+
+    $response = array(
+      'Success' => true,
+      'Info' => 'Data Berhasil di update');
+
+    $this->output
+      ->set_status_header(200)
+      ->set_content_type('application/json', 'utf-8')
+      ->set_output(json_encode($response, JSON_PRETTY_PRINT))
+      ->_display();
+      exit;
+  }
+
+  public function deleteMahasiswa($npm)
+  {
+    $this->Mahasiswa->deleteMahasiswa($npm);
+
+    $response = array(
+      'Success' => true,
+      'Info' => 'Data Berhasil di hapus');
+
+    $this->output
+      ->set_status_header(200)
+      ->set_content_type('application/json', 'utf-8')
+      ->set_output(json_encode($response, JSON_PRETTY_PRINT))
+      ->_display();
+      exit;
+  }
+
+}
